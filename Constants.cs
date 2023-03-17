a755bc91fedf728962fd19442d6aa36b2cdfb400
Saumya Patil <saumyasachinpatil@gmail.com>
2023-03-17 20:22:24 +0530

    Make the constants actually "Global"

--- Constants.cs
+++ Constants.cs
@@ -1,9 +1,18 @@
+// ReSharper disable file InconsistentNaming
+// The "inconsistent naming" is intentional, because they are global constants.
 namespace io.github.DitherWither.counter;
 
 /// <summary>
 /// Contains constants used throughout the application.
 /// </summary>
+///
+/// <remarks>
+/// This class contains constants that are used throughout the application.
+/// This class is `global using`d in Program.cs, so we don't have to specify which class the constant is in.
+///
+/// This is done because C# doesn't have global constants.
+/// </remarks>
 public static class Constants
 {
-    public const string AppId = "io.github.ditherwither.counter";
+    public const string APP_ID = "io.github.ditherwither.counter";
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿using io.github.DitherWither.counter;
+﻿// So that we don't have to specify which class the constant is in. 
+// As all the constants are uppercase, it's easy to tell which are constants and which are not.
+global using static io.github.DitherWither.counter.Constants;
+
 using io.github.DitherWither.counter.ui;
 
-var app = Adw.Application.New(Constants.AppId, Gio.ApplicationFlags.DefaultFlags);
+
+var app = Adw.Application.New(APP_ID, Gio.ApplicationFlags.DefaultFlags);
 app.OnActivate += (sender, _) => {
     var mainWindow = new MainWindow();
 
